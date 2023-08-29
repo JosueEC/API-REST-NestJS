cd25e0765b364f2432e9ee200edf1b52dee09ec9
J Evangelista Cruz <josueev.cruz@gmail.com>
2023-08-28 18:12:30 -0600

    feat(main.ts): 🔨 Add cors configuration

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,11 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: ['http://localhost:5173'],
+    methods: ['HEAD', 'GET', 'PUT', 'PATCH', 'POST', 'DELETE', 'OPTIONS'],
+    credentials: true,
+  });
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(3000);
 }
