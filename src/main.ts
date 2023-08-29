3d1a2148a3ff824b8c87ea07dc78b0c2fa17eca1
J Evangelista Cruz <josueev.cruz@gmail.com>
2023-08-28 18:20:35 -0600

    fix(main.ts): 🔨 Fix cors origin domain

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: ['http://localhost:5173'],
+    origin: ['http://127.0.0.1:5173'],
     methods: ['HEAD', 'GET', 'PUT', 'PATCH', 'POST', 'DELETE', 'OPTIONS'],
     credentials: true,
   });
